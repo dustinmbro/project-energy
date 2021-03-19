5a833ede9323717b741ce417261def27174837c8
Remko-Kraan <74554598+Remko-Kraan@users.noreply.github.com>
2021-03-19 14:05:46 +0100

    FAQ toegevoegd

---
+++ energy/js/accordion.js
@@ -0,0 +1,41 @@
+window.onclick = function(element){
+   var panel = element.target.nextElementSibling;
+
+
+switch(element.target.id){
+    case 'accPanel1':
+        if(panel.style.height == "30px"){
+            panel.style.height = "0px";
+        }else{
+            panel.style.height = "30px";
+        }
+    
+    break;
+    case 'accPanel2':
+        if(panel.style.height == "30px"){
+            panel.style.height = "0px";
+        }else{
+            panel.style.height = "30px";
+        }
+    
+    break;
+    case 'accPanel3':
+        if(panel.style.height == "30px"){
+            panel.style.height = "0px";
+        }else{
+            panel.style.height = "30px";
+        }
+    
+    break;
+    case 'accPanel4':
+        if(panel.style.height == "30px"){
+            panel.style.height = "0px";
+        }else{
+            panel.style.height = "30px";
+        }
+    
+    break;
+       
+}
+
+}
